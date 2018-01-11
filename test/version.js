739aec80cfb36c503bcf575fa0408170020b0df9
Jon Swenson <jswenson74@gmail.com>
2018-01-10 22:44:27 -0500

    Assignment6
    * build ./authenticate and ./private points.
    * use prerequisite extensions to execute authentication logic.
    * Make simple database.js data store to authenticate user records with.
    * Apply default authStrategy to ./private point.
    * No authStrategy for ./authenticate point.

--- test/version.js
+++ test/version.js
@@ -42,4 +42,77 @@ describe('/version', () => {
         expect(res.result).to.equal('version 1.0.5 lesson5');
         await server.stop();
     });
+
+    it('succesfully authenticates', { parallel: false }, async () => {
+
+        const University = require('../lib');
+
+        const server = await University.init(internals.serverOptions);
+
+        expect(server).to.be.an.object();
+
+        // curl -H "Content-Type: application/json" -X POST -d '{"username":"foofoo","password":"12345678"}' https://localhost:8000/authenticate
+
+        const request = { method: 'POST', url: '/authenticate', payload: { username: 'foofoo', password: '12345678' } };
+
+        const res = await server.inject(request);
+
+        expect(res.result.message).to.equal('welcome');
+        expect(res.result.token.length).to.equal(8);
+        await server.stop();
+    });
+
+    it('fails to authenticate, bad password', { parallel: false }, async () => {
+
+        const University = require('../lib');
+
+        const server = await University.init(internals.serverOptions);
+
+        expect(server).to.be.an.object();
+
+        const request = { method: 'POST', url: '/authenticate', payload: { username: 'foofoo', password: '123478' } };
+
+        const res = await server.inject(request);
+
+        expect(res.result.statusCode).to.equal(401);
+        expect(res.result.message).to.equal('invalid credentials');
+        await server.stop();
+    });
+
+    it('fails to authenticate, bad username', { parallel: false }, async () => {
+
+        const University = require('../lib');
+
+        const server = await University.init(internals.serverOptions);
+
+        expect(server).to.be.an.object();
+
+        const request = { method: 'POST', url: '/authenticate', payload: { username: 'tootoo', password: '123478' } };
+
+        const res = await server.inject(request);
+
+        expect(res.result.statusCode).to.equal(401);
+        expect(res.result.message).to.equal('invalid credentials');
+        await server.stop();
+    });
+
+    it('successfully access private data', async () => {
+
+        const University = require('../lib');
+
+        const server = await University.init(internals.serverOptions);
+
+        expect(server).to.be.an.object();
+
+        // curl -k -X GET -H "Authorization: Bearer 12345678" https://localhost:8000/version
+
+        const request = { method: 'GET', url: '/private', headers: { authorization: 'Bearer 12345678' } };
+
+        const res = await server.inject(request);
+
+        expect(res.result).to.equal('privateData');
+        await server.stop();
+    });
 });
+
+
