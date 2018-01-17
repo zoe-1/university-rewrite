8d59921e084a812c29a51afee0991148cdc7ab63
Jon Swenson <jswenson74@gmail.com>
2018-01-16 21:59:30 -0500

    graphi & graphql (for part3)
        * schema now has:
          - one interface: Repositories
          - three types: Hapi, xHapi, Topics.
        * 'xHapi' repositories are repos not under the hapi umbrella.
        * Built tests for multiple searches:
          - simple searches based on repositories id.
          - retrieve related repositories
          - retrieve related repositories of related repositories
            (friend of friends)
          - inlineFragraments retrieve topics if repo is xHapi type.
          - inlineFragraments retrieve topics of related projects if is
            xHapi type.
        * Changed data store to be an array of records.
          - preparing for mutations.

--- test/graphi.js
+++ test/graphi.js
@@ -5,7 +5,7 @@ const Lab = require('lab');
 const { suite, test } = exports.lab = Lab.script();
 
 
-suite('graphi development', () => {
+suite('/graphi', () => {
 
     test('search returns name and description values', async () => {
 
@@ -21,7 +21,16 @@ suite('graphi development', () => {
 
         expect(authRes.result.token.length).to.equal(36);
 
-        const request = { method: 'POST', url: '/graphql', headers: { authorization: 'Bearer ' +  authRes.result.token }, payload: { query:'{ getRepository (id:\"1003\") { name description } }' } };
+        const request = {
+            method: 'POST',
+            url: '/graphql',
+            headers: {
+                authorization: 'Bearer ' +  authRes.result.token
+            },
+            payload: {
+                query:'{ getRepository (id:\"1003\") { name description } }'
+            }
+        };
 
         const res = await server.inject(request);
 
@@ -79,7 +88,7 @@ suite('graphi development', () => {
         await server.stop({ timeout: 4 });
     });
 
-    test.only('search returns xHapi type name and topics', async () => {
+    test('search returns xHapi type name and topics (inline fragment)', async () => {
 
         const University = require('../lib');
 
@@ -93,9 +102,227 @@ suite('graphi development', () => {
 
         expect(authRes.result.token.length).to.equal(36);
 
-        const query = '{ getRepository (id:\"1005\") { name } }';
+        const query = '{ getRepository (id:\"1005\") { name ' +  // returns a xHapiType
+                      '   description                       ' +
+                      '     ... on xHapi {                  ' +
+                      '         topics {                    ' +
+                      '           name                      ' +
+                      '         }                           ' +
+                      '      }                              ' +
+                      '     ... on Hapi {                   ' +
+                      '          related {                  ' +
+                      '           name                      ' +
+                      '         }                           ' +
+                      '      }                              ' +
+                      '  }                                  ' +
+                      '}';
 
-        // const query = '{ getRepository (id:\"1005\") { name ... on xHapi { topics { name } } } }';
+        const request = { method: 'POST', url: '/graphql', headers: { authorization: 'Bearer ' +  authRes.result.token }, payload: {  query } };
+
+        const res = await server.inject(request);
+
+        expect(JSON.parse(res.payload)).to.equal({
+            'data':
+            { 'getRepository':
+                {
+                    'name':  'reactjs',
+                    'description':  'user interface builder',
+                    'topics': [
+                        {
+                            'name': 'front-end development'
+                        },
+                        {
+                            'name': 'bundler'
+                        }
+                    ]
+                }
+            }
+        });
+
+        await server.stop({ timeout: 4 });
+    });
+
+    test('search returns Hapi type name and related (inline fragment)', async () => {
+
+        const University = require('../lib');
+
+        const server = await University.init('test');
+
+        expect(server).to.be.an.object();
+
+        const authenticateRequest = { method: 'POST', url: '/authenticate', payload: { username: 'foofoo', password: '12345678' } };
+
+        const authRes = await server.inject(authenticateRequest);
+
+        expect(authRes.result.token.length).to.equal(36);
+
+        const query = '{ getRepository (id:\"1002\") {      ' +  // returns a HapiType
+                      '   name                              ' +
+                      '   description                       ' +
+                      '     ... on xHapi {                  ' +
+                      '         topics {                    ' +
+                      '           name                      ' +
+                      '         }                           ' +
+                      '      }                              ' +
+                      '     ... on Hapi {                   ' +
+                      '          related {                  ' +
+                      '           name                      ' +
+                      '         }                           ' +
+                      '      }                              ' +
+                      '  }                                  ' +
+                      '}';
+
+        const request = { method: 'POST', url: '/graphql', headers: { authorization: 'Bearer ' +  authRes.result.token }, payload: {  query } };
+
+        const res = await server.inject(request);
+
+        expect(JSON.parse(res.payload)).to.equal({
+            'data':
+            { 'getRepository':
+                {
+                    'name':  'catbox-policy',
+                    'description':  'Configure policies to store and maintain objects in cache.',
+                    'related': [
+                        {
+                            'name': 'hapi'
+                        },
+                        {
+                            'name': 'catbox-redis'
+                        }
+                    ]
+                }
+            }
+        });
+
+        await server.stop({ timeout: 4 });
+    });
+
+    test('search fails bad userid', { parallel: false },async () => {
+
+        const University = require('../lib');
+
+        const server = await University.init('test');
+
+        expect(server).to.be.an.object();
+
+        const authenticateRequest = { method: 'POST', url: '/authenticate', payload: { username: 'foofoo', password: '12345678' } };
+
+        const authRes = await server.inject(authenticateRequest);
+
+        expect(authRes.result.token.length).to.equal(36);
+
+        const request = {
+            method: 'POST',
+            url: '/graphql',
+            headers: {
+                authorization: 'Bearer ' +  authRes.result.token
+            },
+            payload: {
+                query:'{ getRepository (id:\"xxxx\") { name description } }'
+            }
+        };
+
+        const res = await server.inject(request);
+
+        expect(JSON.parse(res.payload)).to.equal({
+            'data':
+            { 'getRepository': null
+            }
+        });
+
+        await server.stop({ timeout: 4 });
+    });
+
+    test('search projects > related_project > and related projects (friends of friends)', async () => {
+
+        const University = require('../lib');
+
+        const server = await University.init('test');
+
+        expect(server).to.be.an.object();
+
+
+        const authenticateRequest = { method: 'POST', url: '/authenticate', payload: { username: 'foofoo', password: '12345678' } };
+
+        const authRes = await server.inject(authenticateRequest);
+
+        expect(authRes.result.token.length).to.equal(36);
+
+        // const query = '{ getRepository (id:\"1005\") { name description related { name } } }';
+
+        const query = '{ getRepository (id:\"1002\") {      ' +  // returns a HapiType
+                      '   name                              ' +
+                      '   description                       ' +
+                      '     related {                       ' +
+                      '       name                          ' +
+                      '       related {                     ' +
+                      '         name                        ' +
+                      '       }                             ' +
+                      '     }                               ' +
+                      '  }                                  ' +
+                      '}';
+
+        const request = { method: 'POST', url: '/graphql', headers: { authorization: 'Bearer ' +  authRes.result.token }, payload: {  query } };
+
+        const res = await server.inject(request);
+
+        expect(JSON.parse(res.payload)).to.equal({
+            'data':
+            { 'getRepository':
+                {
+                    'name':  'catbox-policy',
+                    'description':  'Configure policies to store and maintain objects in cache.',
+                    'related': [
+                        {
+                            'name': 'hapi',
+                            'related': [
+                                { 'name': 'http-auth-bearer-token' },
+                                { 'name': 'catbox-policy' },
+                                { 'name': 'catbox-redis' }
+                            ]
+                        },
+                        {
+                            'name': 'catbox-redis',
+                            'related': [
+                                { 'name': 'hapi' },
+                                { 'name': 'catbox-policy' }
+                            ]
+                        }
+                    ]
+                }
+            }
+        });
+
+        await server.stop({ timeout: 4 });
+    });
+
+    test('search projects > topics > and topics of related projects (friends of friends)', async () => {
+
+        const University = require('../lib');
+
+        const server = await University.init('test');
+
+        expect(server).to.be.an.object();
+
+        const authenticateRequest = { method: 'POST', url: '/authenticate', payload: { username: 'foofoo', password: '12345678' } };
+
+        const authRes = await server.inject(authenticateRequest);
+
+        expect(authRes.result.token.length).to.equal(36);
+
+        const query = '{ getRepository (id:\"1005\") {      ' +  // returns a xHapiType
+                      '   name                              ' +
+                      '   description                       ' +
+                      '     ... on xHapi {                  ' +
+                      '         related {                   ' +
+                      '           name                      ' +
+                      '           ... on xHapi {            ' +
+                      '              topics { name }        ' +
+                      '           }                         ' +
+                      '         }                           ' +
+                      '     }                               ' +
+                      '   }                                 ' +
+                      '}';
 
         const request = { method: 'POST', url: '/graphql', headers: { authorization: 'Bearer ' +  authRes.result.token }, payload: {  query } };
 
@@ -109,10 +336,18 @@ suite('graphi development', () => {
                     'description':  'user interface builder',
                     'related': [
                         {
-                            'name': 'browserify'
+                            'name': 'browserify',
+                            'topics': [
+                                { 'name': 'front-end development' },
+                                { 'name': 'bundler' }
+                            ]
                         },
                         {
-                            'name': 'webpack'
+                            'name': 'webpack',
+                            'topics': [
+                                { 'name': 'front-end development' },
+                                { 'name': 'bundler' }
+                            ]
                         }
                     ]
                 }
